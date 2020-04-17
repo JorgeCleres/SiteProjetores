f93e44a1dc95ea034e5672046f6fc70d11c2a403
jorgeCleres <jrgcleres@hotmail.com>
2020-04-17 15:44:29 -0300

    sistema apresenta mensagem de sucesso ou erro de upload

--- inserirImagens.php
+++ inserirImagens.php
@@ -3,20 +3,9 @@ session_start();
 if(isset($_SESSION['login'])){ ?>
 
 <form method="POST" action="processamentoImagem.php" enctype="multipart/form-data">
-    <input type="text" name="nome"></br>
-    <input type="email" name="email"></br>
     <input type="file" name="foto[]" multiple></br>
     <input type="submit" name="submit" value="enviar">
 </form>
-<?php $msgSucesso = isset($_SESSION['msgSucesso']) ? $_SESSION['msgSucesso'] : '';
-      $msgErro = isset($_SESSION['msgErro']) ? $_SESSION['msgErro'] : '';
-      if (!empty($msgSucesso)) {
-          echo $msgSucesso;
-      }
-      if (!empty($msgErro)){
-          echo $msgErro;
-      }
-      ?>
 <?php }else{ ?>
 
 <h2>deslogado</h2>
--- processamentoImagem.php
+++ processamentoImagem.php
@@ -11,31 +11,25 @@ if(isset($_POST['submit']))//verifica se foi clicado no submit
             if (isset($_FILES['foto']))//verifica se foi enviado uma foto
             {
                 move_uploaded_file($_FILES['foto']['tmp_name'][$i],'images/imagensUpload/'.$nome_arquivo);
-                $_SESSION['msgSucesso'] = "upload feito com sucesso";
-                header('location:inserirImagens.php');
-                return;
             }
+            ?><script>alert('upload feito com sucesso')</script>
+                <meta http-equiv="refresh" content="0; url=inserirImagens.php">
+            <?php
+        
         }elseif($_FILES['foto']['type'][$i] == 'image/jpeg')
         {
-            $nome_arquivo = md5($_FILES['foto']['nome'][$i].rand(1,999)).'.jpeg';
+            $nome_arquivo = md5($_FILES['foto']['name'][$i].rand(1,999)).'.jpeg';
             if(isset($_FILES['foto']))
             {
                 move_uploaded_file($_FILES['foto']['tmp_name'][$i] ,'images/imagensUpload/'.$nome_arquivo);
-                $_SESSION['msgSucesso'] = "upload feito com sucesso";
-                header('location:inserirImagens.php');
-                return;
             }
+            ?><script>alert('upload feito com sucesso')</script>
+                <meta http-equiv="refresh" content="0; url=inserirImagens.php">
+            <?php
         }else {
-            $_SESSION['msgErro'] = "falha no uploado, verifique o formato do arquivo.";
-            header('location:inserirImagens.php');
-            return;
+            ?><script>alert('erro no upload')</script>
+                <meta http-equiv="refresh" content="0;">
+            <?php
         }
-    }
-}
-
-function removeMsgSucesso () : void
-{
-    if(isset($_SESSION['msgSucesso'])){
-        unset($_SESSION['msgSucesso']);
     }
 }
